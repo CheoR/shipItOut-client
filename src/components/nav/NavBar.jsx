a16a750e336df1eb88b70b048ae32ba935f68c51
CheoR <cheo.roman+github@gmail.com>
2021-06-17 11:10:26 -0500

    build: user can log in and log out. Incorrect credentials pops up modal.

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -16,11 +16,11 @@ export const NavBar = (props) => {
                 <Link className="nav-link" to="/profile">Profile</Link>
             </li>
             {
-                (localStorage.getItem("lu_token") !== null) ?
+                (localStorage.getItem("user_token") !== null) ?
                     <li className="nav-item">
                         <button className="nav-link fakeLink"
                             onClick={() => {
-                                localStorage.removeItem("lu_token")
+                                localStorage.removeItem("user_token")
                                 props.history.push({ pathname: "/" })
                             }}
                         >Logout</button>
