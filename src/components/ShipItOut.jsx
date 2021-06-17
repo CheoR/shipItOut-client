0740db0a1bd369371d1be1fa75a39bb165669691
CheoR <cheo.roman+github@gmail.com>
2021-06-17 15:18:04 -0500

    build: user can hit the home page at /.

--- src/components/ShipItOut.jsx
+++ src/components/ShipItOut.jsx
@@ -6,16 +6,23 @@ import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 
+import { Home } from "./home/Home"
+
+
 export const ShipItOut = () => (
     <>
     <Route render={() => {
         if (localStorage.getItem("user_token")) {
             return <>
+            console.log("there is a token");
                 <Route render={NavBar} />
                 <Route render={props => <ApplicationViews {...props} />} />
             </>
         } else {
-            return <Redirect to="/login" />
+            return <>
+                {/* <Route render={NavBar} /> */}
+                <Route render={Home} />
+            </>
         }
     }} />
 
