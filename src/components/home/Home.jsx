341c950722738684dad480be8ef69cdb2b956f64
CheoR <cheo.roman+github@gmail.com>
2022-10-08 15:36:46 -0500

    ref: Convert Home page to MUI

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,48 +1,64 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Box, Button, ButtonGroup, Typography } from '@mui/material'
 
 import logo from '../../assets/images/pugTransport.svg'
-import styles from './Home.module.css'
 
 export const Home = () => {
   return (
-    <>
-      <main className={styles.main}>
-        <div className={styles.infoBox}>
-          <h1 className={styles.title}>ShipItOut</h1>
-          <div className={styles.svgContainer}>
-            <img
-              className={styles.logo}
-              src={logo}
-              alt='Pug Transport Logo'
-            />
-          </div>
-          <div className={styles.textBox}>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
-              debitis neque unde beatae maxime dolor quis.
-            </p>
-            <p>
-              Sunt quia hic modi quod, laboriosam et excepturi quo repellendus
-              harum? Praesentium corrupti nulla aperiam. Eveniet architecto,
-              distinctio iste facilis adipisci ipsum. Rem, aliquam odio. Soluta
-              dolorem laudantium quam iusto delectus architecto eaque pariatur.
-            </p>
-          </div>
-        </div>
-        {/* className={`${styles.description} ${styles.yellow}`} */}
-
-        <div className={styles.buttonBox}>
-          <Link to='/login'>
-            <button className={`${styles.btn} ${styles.login}`}>Login</button>
-          </Link>
-          <Link to='/register'>
-            <button className={`${styles.btn} ${styles.register}`}>
-              Register
-            </button>
-          </Link>
-        </div>
-      </main>
-    </>
+    <Box
+      sx={{
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+      }}
+    >
+      <Box>
+        <Typography
+          variant='h1'
+          sx={{ textAlign: 'center' }}
+        >
+          ShipItOut
+        </Typography>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <img
+          src={logo}
+          alt='Pug Transport Logo'
+        />
+      </Box>
+      <Box>
+        <Typography
+          variant='body1'
+          sx={{ textAlign: 'center' }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+          debitis neque unde beatae maxime dolor quis.
+        </Typography>
+        <Typography
+          variant='body2'
+          sx={{ textAlign: 'center' }}
+        >
+          Sunt quia hic modi quod, laboriosam et excepturi quo repellendus
+          harum? Praesentium corrupti nulla aperiam. Eveniet architecto,
+          distinctio iste facilis adipisci ipsum. Rem, aliquam odio. Soluta
+          dolorem laudantium quam iusto delectus architecto eaque pariatur.
+        </Typography>
+      </Box>
+      <ButtonGroup
+        variant='contained'
+        aria-label='outlined primary button group'
+        sx={{ gap: 1, margin: '0 auto' }}
+        size='large'
+      >
+        <Link to='login'>
+          <Button>Login</Button>
+        </Link>
+        <Link to='/register'>
+          <Button>Register</Button>
+        </Link>
+      </ButtonGroup>
+    </Box>
   )
 }
