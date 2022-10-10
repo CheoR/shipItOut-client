bf935a5649bdd3b113131a159fcf71e470f88f2f
CheoR <cheo.roman+github@gmail.com>
2022-10-10 14:44:59 -0500

    ref: cleanup

--- src/components/helpers/Loading.jsx
+++ src/components/helpers/Loading.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import { Box, Typography } from '@mui/material'
 import logo from '../../assets/images/pugTransport.svg'
 
-export const Loading = () => {
-  return (
-    <Box>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <img
-          src={logo}
-          alt='Rotating compoany logo'
-        />
-      </Box>
-      <Typography
-        variant='body1'
-        textAlign='center'
-      >
-        Loading . . .
-      </Typography>
+export const Loading = () => (
+  <Box>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <img
+        src={logo}
+        alt='Rotating compoany logo'
+      />
     </Box>
-  )
-}
+    <Typography
+      variant='body1'
+      textAlign='center'
+    >
+      Loading . . .
+    </Typography>
+  </Box>
+)
