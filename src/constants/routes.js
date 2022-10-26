640d68af0abff4a6446571f299599b95c0d98bcc
CheoR <cheo.roman+github@gmail.com>
2022-10-26 13:30:12 -0500

    build: construct routes to import URL constants

---
+++ src/constants/routes.js
@@ -0,0 +1,13 @@
+export const URL = {
+  BOOKINGS: '/bookings',
+  BOOKING_STATUSES: '/bookings/booking_statuses',
+  CONTAINER_TYPES: '/containers/container_types',
+  CONTAINER_LOCATIONS: '/containers/container_locations',
+  CONTAINERS_AVAILABLE: '/containers/available_containers',
+  HOME: '/',
+  JUST_CARRIERS: '/appusers/just_carriers',
+  LOGIN: '/login',
+  PORTS: '/ports',
+  REGISTER: '/register',
+  VOYAGES: '/voyages',
+}
